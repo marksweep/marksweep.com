e63808b283ac3f1342b43052277d3bb16570611c
David Wu <david@marksweep.com>
2011-07-13 07:09:31 -0400

    Added measures saving first cut to pellucidLoad
    Signed-off-by: David Wu <david@marksweep.com>

--- app/extensions/command/PellucidLoad.php
+++ app/extensions/command/PellucidLoad.php
@@ -12,6 +12,7 @@ use app\models\Measures;
 use app\models\Footnotes;
 use app\models\FootnotesValues;
 use app\models\PellucidEntities;
+use app\models\PellucidMeasures;
 use \lithium\util\Set;
 
 class PellucidLoad extends \lithium\console\Command {
@@ -24,7 +25,9 @@ class PellucidLoad extends \lithium\console\Command {
 	 */
 	public function run($command = null) {
 		
-		$this->loadHospitals($command);
+		// TODO Make this splittable by command so you can run multiple chunks on 
+		// different threads
+		//$this->loadHospitals($command);
 		
 		$this->loadMeasures($command);
 		
@@ -33,16 +36,40 @@ class PellucidLoad extends \lithium\console\Command {
 	
 	/*! ----- Load Measures ----- */
 	
+	
+	/**
+	 * loadMeasures function.
+	 * 
+	 * @access public
+	 * @param mixed $command (default: null)
+	 * @return void
+	 */
 	public function loadMeasures($command = null) {
 	
+
 		// Find all measures
 		$measures = Measures::all();
+		$count = Measures::count();
+		echo 'Count: ' . $count . "\n";
+		echo 'Start measures loop: ' . time() . "\n";
 		
 		// Attach measure groups
 		
 		// Loop through the measures and get the latest value for
 		// every entity that reports on this measure
 		foreach ($measures as $measure) {
+			
+			// Exists?
+			$pellucid_measure = PellucidMeasures::first(array('conditions'=>array('_id'=>$measure->measure_id)));
+			if (!$pellucid_measure) {
+				$pellucid_measure = PellucidMeasures::create();
+			}
+			
+			$data = $measure->data();
+			$data["_id"] = $measure->measure_id;
+			$pellucid_measure->save($data);
+
+			$data = null;
 		
 			// Find entities for this measure
 			$values = EntityValues::all(
@@ -65,33 +92,40 @@ class PellucidLoad extends \lithium\console\Command {
 			foreach ($entity_ids as $entity_id) {
 			
 				// Grab entity information
-				$entity = PellucidEntity::first(array(
+				$entity = PellucidEntities::first(array(
 					'conditions' => array(
-						'_id' => $entity_id
+						'_id' => "$entity_id"
 					)
 				));
+				
+				echo "Entity id: $entity_id \n";
+				//print_r($entity->data());
 			
 				// Get 8 latest values and attach footnotes
-				$values = $this->loadValues(array('measure_id' => $measure->measure_id, 'entity_id' => $entity_id), array('limit'=>8));
-				$all_entities[$entity_id]['Entity'] = $entity->data();
-				$all_entities[$entity_id]['Values'] = $values;
+				$values = $this->loadValues(array('measure_id' => $measure->measure_id, 'entity_id' => $entity_id), array('limit'=>2));
+				print_r($values);
+				
+				// Save incrementally to the current measure
+				PellucidMeasures::update(
+					array('Entities' => array(
+						$entity->data() + array('Values' => $values)
+					)),
+					array('_id' => $measure->measure_id)
+				);
+				
+				//$all_entities[$entity_id] = $entity->data();
+				//$all_entities[$entity_id]['Values'] = $values;
 				
 			
 			}
-			// Exists?
-			$pellucid_measure = PellucidMeasure::first(array('conditions'=>array('_id'=>$measure->measure_id)));
-			if (!$pellucid_measure) {
-				$pellucid_measure = PellucidMeasures::create();
-			}
 			
-			$data = $measure->data();
-			$data['Entities'] = $all_entities;
+			//$data['Entities'] = $all_entities;
 			
-			$pellucid_measure->save($data);
-		
+			break;
 		}
 	
-	
+		
+		echo 'End measures loop: ' . time() . "\n";
 	}
 	
 	/*!----- Load Hospitals ----- */
@@ -141,7 +175,6 @@ class PellucidLoad extends \lithium\console\Command {
 			$data['Measures'] = $this->loadValues(array('entity_id' => $data['_id']), array('attachMeasures'=>true));
 			
 			$entity->save($data);
-			echo 'Hospital: ' . $data['entity_id'] . "\n";
 		}
 		echo 'End hospital loop: ' . time() . "\n";
 	
@@ -157,7 +190,7 @@ class PellucidLoad extends \lithium\console\Command {
 	 * @return void
 	 */
 	protected function loadValues($conditions = array(), $options = array()) {
-
+		print_r($options);
 		// Parse some options
 		$limit = null;
 		if (isset($options['limit'])) {
@@ -177,13 +210,18 @@ class PellucidLoad extends \lithium\console\Command {
 		
 		// Create date_end/date_start for all the values
 		$latest_date_end = null;
+		$latest_date_end_value = null;
 		
 		foreach($values as $id => $value){
 			$indexed_values[$id] = $value->data();
 			$indexed_values[$id]['date_range'] = $indexed_values[$id]['date_end'] . '|' . $indexed_values[$id]['date_start'];
 			
 			// Track the latest date_end found
-			$latest_date_end = (($indexed_values[$id]['date_end'] > $latest_date_end) ? $indexed_values[$id]['date_end'] : $latest_date_end);
+			if ($indexed_values[$id]['date_end'] > $latest_date_end) {
+				$latest_date_end = $indexed_values[$id]['date_end'];
+				$latest_date_end_value = $indexed_values[$id];
+			
+			}
 			
 			// Find and attach footnotes
 			$footnotes = FootnotesValues::all(
@@ -200,20 +238,21 @@ class PellucidLoad extends \lithium\console\Command {
 			$indexed_values[$id]['Footnotes'] = $footnotes->data();
 		}
 		
-		
-		$measure_ids = array();
-		if (count($indexed_values) > 0) {
-			$combined_values = Set::combine($indexed_values, '/date_range', '/', '/measure_id');
-			
-			// Grab measures related
-			$measure_ids = array_keys($combined_values);
-		}
-		
-		
-		
-		$all_values = array();
-		
+		// If we are grouping be measures (attachMeasures)
+		// Then we organize our values by measure_id
 		if (isset($options['attachMeasures'])) {
+			$measure_ids = array();
+			if (count($indexed_values) > 0) {
+				$combined_values = Set::combine($indexed_values, '/date_range', '/', '/measure_id');
+				
+				// Grab measures related
+				$measure_ids = array_keys($combined_values);
+			}
+			
+			
+			
+			$all_values = array();
+		
 			// Cache these measure_ids;
 			foreach ($measure_ids as $measure_id) {
 			
@@ -232,12 +271,16 @@ class PellucidLoad extends \lithium\console\Command {
 				);
 				
 				// Measures are large array
-				$all_values[] = $measure->data() + array('Values' => $combined_values[$measure_id]);
+				$all_values[] = $measure->data() + array('Values' => $combined_values[$measure_id]) + array('Latest' => $latest_date_end_value);
 				
 			}
+			
+		// If we are not grouping by measures
+		// This is the DEFAULT
 		} else {
 			$all_values[] = array(
-				'Values' => $combined_values
+				'Values' => $indexed_values,
+				'Latest' => $latest_date_end_value
 			);
 		}
 		
